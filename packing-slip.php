1472a679e460421c87b87006998fcac7808b73cf
Sergey Kariy <sergeyk@wp-server.wp-server.ru>
2017-04-13 11:55:41 +0300

    v2

--- packing-slip.php
+++ packing-slip.php
@@ -1,81 +1,58 @@
-<?php global $wpo_wcpdf; ?>
+<?php 
+global $wpo_wcpdf;
+//пролучение полного пути
+$dir = plugin_dir_path( __FILE__ );
+//получаем название компании заказчика
+$name_company = $wpo_wcpdf->export->order->data['billing']['company'];
+ ?>
 <?php do_action( 'wpo_wcpdf_before_document', $wpo_wcpdf->export->template_type, $wpo_wcpdf->export->order ); ?>
-
-<table class="head container">
-	<tr>
-		<td class="header">
-		<?php
+   <header class="txt-right">
+       <?php
 		if( $wpo_wcpdf->get_header_logo_id() ) {
 			$wpo_wcpdf->header_logo();
 		} else {
 			echo apply_filters( 'wpo_wcpdf_packing_slip_title', __( 'Packing Slip', 'wpo_wcpdf' ) );
 		}
 		?>
-		</td>
-		<td class="shop-info">
-			<div class="shop-name"><h3><?php $wpo_wcpdf->shop_name(); ?></h3></div>
-			<div class="shop-address"><?php $wpo_wcpdf->shop_address(); ?></div>
-		</td>
-	</tr>
-</table>
-
-<h1 class="document-type-label">
-<?php if( $wpo_wcpdf->get_header_logo_id() ) echo apply_filters( 'wpo_wcpdf_packing_slip_title', __( 'Packing Slip', 'wpo_wcpdf' ) ); ?>
-</h1>
-
+    </header>
+    <h2 class="brd-bott" align="center">АКТ № <?php $wpo_wcpdf->invoice_number(); ?> от <?php $wpo_wcpdf->order_date(); ?></h2>
+    <br>
+    <section class="brd-bott">
+    	<table>
+    		<tr>
+    			<td>Исполнитель:</td>
+    			<td><h4>Индивидуальный предприниматель Никитин Иван Геннадьевич</h4></td>
+    		</tr>
+    		<tr>
+    			<td>Заказчик:</td>
+    			<td><h4><?php echo $name_company;?></h4></td>
+    		</tr>
+    	</table>
+    <br>
 <?php do_action( 'wpo_wcpdf_after_document_label', $wpo_wcpdf->export->template_type, $wpo_wcpdf->export->order ); ?>
 
-<table class="order-data-addresses">
-	<tr>
-		<td class="address shipping-address">
-			<!-- <h3><?php _e( 'Shipping Address:', 'wpo_wcpdf' ); ?></h3> -->
-			<?php $wpo_wcpdf->shipping_address(); ?>
-			<?php if ( isset($wpo_wcpdf->settings->template_settings['packing_slip_email']) ) { ?>
-			<div class="billing-email"><?php $wpo_wcpdf->billing_email(); ?></div>
-			<?php } ?>
-			<?php if ( isset($wpo_wcpdf->settings->template_settings['packing_slip_phone']) ) { ?>
-			<div class="billing-phone"><?php $wpo_wcpdf->billing_phone(); ?></div>
-			<?php } ?>
-		</td>
-		<td class="address billing-address">
-			<?php if ( isset($wpo_wcpdf->settings->template_settings['packing_slip_billing_address']) && $wpo_wcpdf->ships_to_different_address()) { ?>
-			<h3><?php _e( 'Billing Address:', 'wpo_wcpdf' ); ?></h3>
-			<?php $wpo_wcpdf->billing_address(); ?>
-			<?php } ?>
-		</td>
-		<td class="order-data">
-			<table>
-				<?php do_action( 'wpo_wcpdf_before_order_data', $wpo_wcpdf->export->template_type, $wpo_wcpdf->export->order ); ?>
-				<tr class="order-number">
-					<th><?php _e( 'Order Number:', 'wpo_wcpdf' ); ?></th>
-					<td><?php $wpo_wcpdf->order_number(); ?></td>
-				</tr>
-				<tr class="order-date">
-					<th><?php _e( 'Order Date:', 'wpo_wcpdf' ); ?></th>
-					<td><?php $wpo_wcpdf->order_date(); ?></td>
-				</tr>
-				<tr class="shipping-method">
-					<th><?php _e( 'Shipping Method:', 'wpo_wcpdf' ); ?></th>
-					<td><?php $wpo_wcpdf->shipping_method(); ?></td>
-				</tr>
-				<?php do_action( 'wpo_wcpdf_after_order_data', $wpo_wcpdf->export->template_type, $wpo_wcpdf->export->order ); ?>
-			</table>			
-		</td>
-	</tr>
-</table>
+<table class="brd txt-center">
+		<thead>
+	        <tr>
+	           <td>№</td>
+	            <td>Наименование работ, услуг</td>
+	            <td class="nbsp">Кол-во</td>
+	            <td>Цена</td>
+	            <td>Сумма</td>
+	          </tr>
+		</thead>
+		<tbody>
+		<?php 
+		//вывод корзины
 
-<?php do_action( 'wpo_wcpdf_before_order_details', $wpo_wcpdf->export->template_type, $wpo_wcpdf->export->order ); ?>
+		$i = 1; //счётник
+		$items = $wpo_wcpdf->get_order_items(); if( sizeof( $items ) > 0 ) : foreach( $items as $item_id => $item ) :
+		 ?>
 
-<table class="order-details">
-	<thead>
-		<tr>
-			<th class="product"><?php _e('Product', 'wpo_wcpdf'); ?></th>
-			<th class="quantity"><?php _e('Quantity', 'wpo_wcpdf'); ?></th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php $items = $wpo_wcpdf->get_order_items(); if( sizeof( $items ) > 0 ) : foreach( $items as $item_id => $item ) : ?>
 		<tr class="<?php echo apply_filters( 'wpo_wcpdf_item_row_class', $item_id, $wpo_wcpdf->export->template_type, $wpo_wcpdf->export->order, $item_id ); ?>">
+			<td>
+				<?php echo $i++; ?>
+			</td>
 			<td class="product">
 				<?php $description_label = __( 'Description', 'wpo_wcpdf' ); // registering alternate label translation ?>
 				<span class="item-name"><?php echo $item['name']; ?></span>
@@ -89,26 +66,39 @@
 				<?php do_action( 'wpo_wcpdf_after_item_meta', $wpo_wcpdf->export->template_type, $item, $wpo_wcpdf->export->order  ); ?>
 			</td>
 			<td class="quantity"><?php echo $item['quantity']; ?></td>
+			<td class="price"><?php echo $item['price']; ?></td>
+			<td class="price"><?php echo $item['price'];?></td>
 		</tr>
 		<?php endforeach; endif; ?>
 	</tbody>
-</table>
+	</table>
+	<?php
+		//получаем итоговую цену
+	$price = $wpo_wcpdf->get_woocommerce_totals();
+		//получаем нужные нам данные
+	$price_sub = $price['order_total']['value'];
+		//
+	$price_natual = price_delimiter($price_sub);
+	
+	?>
+           <table width="100%">
+           		<tr>
+           			<td width="60%"></td>
+           			<td width="40%">
+           				<table width="100%">
+           					<tr>
+           					<td align="center"><h4>Итого к оплате:</h4></td>
+           					<td align="right"><h4><?php echo $price['order_total']['value']; ?></h4></td>
+           					</tr>
+           				</table>
+           		</tr>
+           </table> 
+           <br>
+           <br>          
+             <p>Общая стоимость выполненных работ, оказанных услуг: <?php echo number_in_words($price_natual['price']); ?>  </p>
+             <hr>
 
-<?php do_action( 'wpo_wcpdf_after_order_details', $wpo_wcpdf->export->template_type, $wpo_wcpdf->export->order ); ?>
-
-<?php do_action( 'wpo_wcpdf_before_customer_notes', $wpo_wcpdf->export->template_type, $wpo_wcpdf->export->order ); ?>
-<div class="customer-notes">
-	<?php if ( $wpo_wcpdf->get_shipping_notes() ) : ?>
-		<h3><?php _e( 'Customer Notes', 'wpo_wcpdf' ); ?></h3>
-		<?php $wpo_wcpdf->shipping_notes(); ?>
-	<?php endif; ?>
+ <div class="mrgLeft0 padTop clearfix">
+<p>Заказчик не имеет претензий по срокам, качеству и объему товаров и услуг.
+Счет выставлен на основании договора No 2016/24 от 21.07.2016. Тариф "Консалтинг. Месячный абонемент"</p>
 </div>
-<?php do_action( 'wpo_wcpdf_after_customer_notes', $wpo_wcpdf->export->template_type, $wpo_wcpdf->export->order ); ?>
-
-<?php if ( $wpo_wcpdf->get_footer() ): ?>
-<div id="footer">
-	<?php $wpo_wcpdf->footer(); ?>
-</div><!-- #letter-footer -->
-<?php endif; ?>
-
-<?php do_action( 'wpo_wcpdf_after_document', $wpo_wcpdf->export->template_type, $wpo_wcpdf->export->order ); ?>
